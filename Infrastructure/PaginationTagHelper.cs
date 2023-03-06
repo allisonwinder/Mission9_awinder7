081ce455dabffb688f90e266fe2ce8d80ede3955
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2023-03-06 08:16:03 -0700

    = sign

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -44,7 +44,7 @@ namespace Mission9_awinder7.Infrastructure
             IUrlHelper uh = uhf.GetUrlHelper(vc);
             TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i <PageBlah.TotalPages; i++)
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
